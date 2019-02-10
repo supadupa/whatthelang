be08404dbb40703b17680ae82c42089e8c2e08c6
Doug Friedman <sillyville+github@gmail.com>
2019-02-10 11:38:40 -0500

    initial tests working
    migrating pyfasttext syntax to fastText syntax

--- whatthelang/predict_lang.py
+++ whatthelang/predict_lang.py
@@ -1,4 +1,4 @@
-from pyfasttext import FastText
+import fastText
 from os import path
 import re
 
@@ -11,18 +11,18 @@ class WhatTheLang(object):
         self.unknown = "CANT_PREDICT"
 
     def load_model(self):
-        return FastText(self.model_file)
+        return fastText.load_model(self.model_file)
 
     def _clean_up(self,txt):
         txt = re.sub(r"\b\d+\b", "", txt)
         return txt
 
     def _flatten(self,pred):
-        return [item[0] if len(item)!=0 else self.unknown for item in pred]
+        return [re.sub('__label__', '', item[0]) if len(item)!=0 else self.unknown for item in pred[0]]
 
 
     def _get_langs(self):
-        return self.model.labels
+        return self.model.get_labels()
 
 
     def predict_lang(self,inp):
@@ -30,9 +30,12 @@ class WhatTheLang(object):
             cleaned_txt = self._clean_up(inp)
             if cleaned_txt == "":
                 raise ValueError("Not enough text to predict language")
-            pred = self.model.predict([cleaned_txt])[0]
+            pred = self.model.predict([cleaned_txt])[0][0]
             if len(pred) == 0:
                 return self.unknown
+            else:
+                # need to remove __label__ from labels
+                pred[0] = re.sub('__label__', '', pred[0])
             return pred[0]
         else:
             batch = [self._clean_up(i) for i in inp]
